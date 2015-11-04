dd2ab8c7cab05ba71eab15160abd44a583b621a0
ricard <ricard@ricard-H81M-D3H>
2015-11-04 20:39:16 +0100

    ja he acabat!

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -5,6 +5,17 @@
  */
 package pr7app;
 
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 /**
  *
  * @author ivan
@@ -14,10 +25,29 @@ public class Pr7app {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Hi world!");
+    public static void main(String[] args) throws MalformedURLException, IOException, IllegalStateException, FTPIllegalReplyException, FTPException, FileNotFoundException, FTPDataTransferException, FTPAbortedException {
+       // TODO code application logic here
+
+        URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
+        url.openConnection();
+
+        InputStream reader = url.openStream();
+        FileOutputStream writer = new FileOutputStream("/home/ricard/Escritorio/ricardlafuente00");
+        byte[] buffer = new byte[153600];
+        int bytesRead = 0;
+        while ((bytesRead = reader.read(buffer)) > 0) {
+            writer.write(buffer, 0, bytesRead);
+            buffer = new byte[153600];
+        }
+        writer.close();
+        reader.close();
+
+        FTPClient client = new FTPClient();
+        client.connect("srv.toca.cat");
+        client.login("fulano", "Platano123");
+        client.upload(new java.io.File("/home/ricard/Escritorio/ricardlafuente00"));
+        client.disconnect(true);
+
     }
     
 }
